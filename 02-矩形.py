ffc0de23a2306175f3aa07963deee5417a308060
dongdong5820 <407591245@qq.com>
2019-04-23 23:04:28 -0700

    输出矩形

---
+++ 02-矩形.py
@@ -0,0 +1,15 @@
+while True :
+	num = input("请输入一个正整数：")
+	if num.isdigit() and int(num) > 0:
+		num = int(num)
+		break
+
+i = 1
+while i <= num :
+	j = 1
+	while j <= num :
+		print("*",end='')
+		j+=1
+	print()
+	i+=1
+print("矩形输出完成!!!")	 
