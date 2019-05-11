88fccab9e1cacb20d881e9e1cb6b0650fb65a032
suchangdong <suchangdong@globalegrow.com>
2019-05-11 10:12:14 +0800

    作用域和命名空间

---
+++ 10-作用域与命名空间.py
@@ -0,0 +1,22 @@
+def scope_test():
+	def do_local():
+		spam = "local spam"
+
+	def do_nonlocal():
+		nonlocal spam
+		spam = "nonlocal spam"
+
+	def do_global():
+		global spam
+		spam = "global spam"
+
+	spam = "test spam"
+	do_local()
+	print("After local assignment:%s"%spam)
+	do_nonlocal()
+	print("After nonlocal assignment:%s"%spam)
+	do_global()
+	print("After global assignment:%s"%spam)
+
+scope_test()
+print("In global scope:%s"%spam)
