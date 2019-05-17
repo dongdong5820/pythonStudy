e36f96434a8fd25ea789a15baf1fe26905aa5cb0
suchangdong <suchangdong@globalegrow.com>
2019-05-17 15:18:17 +0800

    修改.gitigonre文件,斐波拉契生成器

--- generators.py
+++ generators.py
@@ -8,10 +8,30 @@ def gen_expr(num):
 def gen_fun(num):
 	x = 0
 	while x < num:
-		yield x**2	#yield(放弃)，函数在此地方交出CPU控制权(停止运行)。 在调用next(),send()方法时再往下执行
-		x+=1
-	return 
+		a = yield x**2	#yield(放弃)，函数在此地方交出CPU控制权(停止运行)。 在调用next(),send()方法时再往下执行
+		if a == 100:	#上行的a可以接受send(100)函数的的传参.如100
+			print('I am 100')
+		else:
+			x+=1
 
+#裴波拉切generator
+def gen_fib(num):
+	x = 0
+	a,b = 1,1
+	while x < num:
+		yield b
+		a,b = b,a+b 
+		x+=1
+
+#直接执行此文件时如下代码会执行。若以模块形式导入该文件时如下代码不执行。 主要用于测试
 if __name__ == '__main__':
 	g1 = gen_fun(5)
 	print(g1)
+	print(g1.__next__())
+	g1.send(100)
+	print(g1.__next__())
+	print('-'*50)
+	g2 = gen_fib(5)
+	print(g2)
+	for i in g2:
+		print(i)
--- oop/01-对象基本概念.py
+++ oop/01-对象基本概念.py
@@ -1,22 +1,23 @@
-# 定义一个类
 class Person(object):
-    # 创建对象时调用
-    def __new__(cls, name, sex):
-        print('调用自己的构造方法创建对象')
-        return object.__new__(cls)
+	# 创建对象时调用
+	def __new__(cls, name, sex):
+		print('调用自己的构造方法创建对象')
+		return object.__new__(cls)
 
-    # 初始化方法
-    def __init__(self, name, sex):
-        self.name = name
-        self.sex = sex
+	# 初始化方法
+	def __init__(self, name, sex):
+		self.name = name
+		self.sex = sex
 
-    # 打印类的对象时调用，该方法必须返回一个字符串
-    def __str__(self):
-        return '我是%s,我的性别%s' % (self.name, self.sex)
+	# 打印类的对象时调用，该方法必须返回一个字符串
+	def __str__(self):
+		return '我是%s,我的性别%s' % (self.name, self.sex)
 
-    # 析构函数，当类的对象的所有引用被删除时调用
-    def __del__(self):
-        print('%s对象引用全被删除，执行析构函数' % self.name)
+	# 析构函数，当类的对象的所有引用被删除时调用
+	def __del__(self):
+		print('%s对象引用全被删除，执行析构函数' % self.name)
+
+
 # __new__,__init__,__str__,__del__已两个下划线开始，两个下划线结束的方法都叫魔术方法，不会调用，程序会在特定情况下自动调用
 
 
@@ -26,6 +27,6 @@ p3 = p1
 print(id(p1))
 print(id(p2))
 print(id(p3))
-del(p1)
+del (p1)
 print(p2)
-del(p3)
+del (p3)
