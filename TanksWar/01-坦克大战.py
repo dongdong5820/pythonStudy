d4df1458e7103e50f3148950a2aca801506e5d9b
dongdong5820 <407591245@qq.com>
2019-05-29 11:27:13 +0000

    坦克大战

--- TanksWar/01-坦克大战.py
+++ TanksWar/01-坦克大战.py
@@ -10,6 +10,7 @@ class TankMain():
         pygame.init()
         #创建一个屏幕，屏幕（窗口）的大小，窗口的特性
         screen = pygame.display.set_mode((600,500),0,32)
+        #设置主屏幕标题
         pygame.display.set_caption('坦克大战')
         while True:
             #设置屏幕的背景色为黑色
@@ -51,6 +52,30 @@ class TankMain():
         font = pygame.font.SysFont("华文宋体",20)
         text_sf = font.render("敌方坦克数量为：5",True,(255,0,0))
         return text_sf
+
+"""坦克大战所有对象的父类"""
+class BaseItem(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        #所有对象功能的属性
+
+class Tank(BaseItem):
+    width=50
+    height=50
+    def __init__(self,screen):
+        super().__init__(self)
+        self.screen=screen
+        self.derection="D" #坦克方向，默认往下(上下左右)
+        self.images={} #坦克所有图片
+        self.images["L"] = pygame.image.load("images/tankL.gif")
+        self.images["R"] = pygame.image.load("images/tankR.gif")
+        self.images["U"] = pygame.image.load("images/tankU.gif")
+        self.images["D"] = pygame.image.load("images/tankD.gif")
+        self.image=self.images[self.derection]#坦克图片由方向决定
+        self.live=True#决定坦克是否消灭了
+
+    def display(self):
+        self.screen.blit()
 
 
 game = TankMain()
